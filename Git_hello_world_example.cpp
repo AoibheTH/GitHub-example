221c0b86372507c292ec3e27a5cc3c2cec80b59e
Aoibhe Turner-Heaney <aoibhe.turner@resmed.com>
2020-02-11 17:18:52 +0000

    Output string added

--- Git_hello_world_example.cpp
+++ Git_hello_world_example.cpp
@@ -4,7 +4,8 @@ using namespace std;
 main()
 {
     cout << "Hello World!" << endl; // output string
-
+    cout << "Added an output" << endl; // additional output string added
+    
     return 0;
 
 }
